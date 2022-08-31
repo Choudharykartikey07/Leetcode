20a82b7cf67eeb9d54ba99be8f72eb7a9f97aba6
Choudharykartikey07 <100362230+Choudharykartikey07@users.noreply.github.com>
2022-08-31 16:35:46 +0530

    Create 1773.cpp

---
+++ 1773.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/count-items-matching-a-rule/
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int j,count=0;
+        if(ruleKey == "type")            
+            j=0;
+        else if(ruleKey == "color")
+            j=1;   
+        else
+            j=2;
+        for(int i=0;i<items.size();i++){
+            if(items[i][j]== ruleValue){
+                count++;
+            }
+        }
+        return count;
+                
+            
+        
+    }
+};
