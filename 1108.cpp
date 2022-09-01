c7a12e7e1a9998792019169bb2491e54246395c1
Choudharykartikey07 <100362230+Choudharykartikey07@users.noreply.github.com>
2022-09-01 23:05:57 +0530

    Create 1108.cpp

---
+++ 1108.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/defanging-an-ip-address/
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        for(int i=0;i<address.size();i++){
+            if(address[i]=='.'){
+                s += "[.]";
+            
+            }
+            else{
+                s += address[i];
+            }
+        }
+        return s;
+        
+    }
+};
