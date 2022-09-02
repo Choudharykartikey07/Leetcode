570b5aec2202c6510a457c63f39d5b16bec21005
Choudharykartikey07 <100362230+Choudharykartikey07@users.noreply.github.com>
2022-09-02 22:08:42 +0530

    Create 58.cpp

---
+++ 58.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/length-of-last-word/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        for(int i=s.size()-1;i>=0;--i){
+            if(s[i]==' ' && length>0)
+                return length;
+            if(s[i]!=' ')
+                length++;
+        }
+        return length;
+    }
+    
+};
