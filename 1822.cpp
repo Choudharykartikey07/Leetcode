385ef35a99a3974e1c303604558cc7b922e72ac2
Choudharykartikey07 <100362230+Choudharykartikey07@users.noreply.github.com>
2022-09-11 22:04:28 +0530

    Create 1822.cpp

---
+++ 1822.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/sign-of-the-product-of-an-array/
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                return 0;
+            else if(nums[i]<0){
+                count ++;
+            }
+        }
+        if(count%2==0)
+            return 1;
+        else
+            return -1;
+    }
+        
+};
